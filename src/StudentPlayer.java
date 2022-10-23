23bcb06810d907c558665b732caa35a8e9f4cae1
Dancs Balázs <100741583+dancsb@users.noreply.github.com>
2022-10-23 04:13:39 +0200

    idk man

--- src/StudentPlayer.java
+++ src/StudentPlayer.java
@@ -1,12 +1,17 @@
+import java.util.Random;
+
 public class StudentPlayer extends Player{
+
+    private final static int MAX_DEPTH = 12;
+    private int result = 0;
+    private Random random;
+
     public StudentPlayer(int playerIndex, int[] boardSize, int nToConnect) {
         super(playerIndex, boardSize, nToConnect);
+        random = new Random();
     }
 
-    private final static int MAX_DEPTH = 10;
-    private int result = 0;
-
-    private boolean is3inCol(Board board) {
+    private boolean is3InCol(Board board) {
         if (board.getLastPlayerRow() <= 3 && board.getLastPlayerRow() > 0) {
             for (int i = board.getLastPlayerRow(); i < board.getLastPlayerRow() + 3; i++)
                 if (board.getState()[i][board.getLastPlayerColumn()] != board.getLastPlayerIndex())
@@ -16,18 +21,39 @@ public class StudentPlayer extends Player{
         return false;
     }
 
-    private int score(Board board) {
-        if (board.getWinner() == 2) return 10000000;
-        if (board.getWinner() == 1) return -10000000;
-        if (board.getWinner() == 0) return 0;
-        if (is3inCol(board)) {
-            if(board.getLastPlayerIndex() == 2) return 50;
-            if(board.getLastPlayerIndex() == 1) return -50;
+    private int getTopRowIndex(Board board, int col) {
+        for (int row = 0; row < boardSize[0] ; row++)
+            if (board.getState()[row][col] != -1)
+                return row;
+        return boardSize[0];
+    }
+
+    private boolean is3InCol(Board board, int playerIndex) {
+        for (int col = 0; col < 7; col++) {
+            int topRowIndex = getTopRowIndex(board, col);
+            if (topRowIndex < boardSize[0] - nToConnect + 1 && topRowIndex > 0) {
+                boolean fail = false;
+                for (int row = topRowIndex; row < topRowIndex + nToConnect - 1; row++)
+                    if (board.getState()[row][col] != playerIndex) {
+                        fail = true;
+                        break;
+                    }
+                if (!fail) return true;
+            }
         }
-        if (board.getLastPlayerColumn() == 3) {
+        return false;
+    }
+
+    private int score(Board board) {
+        if (board.getWinner() == 2) return 100;
+        if (board.getWinner() == 1) return -100;
+        if (board.getWinner() == 0) return 0;
+        if (is3InCol(board, 1)) return -50;
+        if (is3InCol(board, 2)) return 50;
+        /*if (board.getLastPlayerColumn() == 3) {
             if(board.getLastPlayerIndex() == 2) return 10;
             if(board.getLastPlayerIndex() == 1) return -10;
-        }
+        }*/
         return 0;
     }
 
@@ -41,9 +67,11 @@ public class StudentPlayer extends Player{
                 Board nextBoard = new Board(board);
                 nextBoard.step(2, board.getValidSteps().get(p));
                 int value = minimax(nextBoard, depth - 1, alpha, beta, false);
+                /*if (value == 0)
+                    value = minimax(board, depth - 1, alpha, beta, false);*/
                 if(depth == MAX_DEPTH && value > maxValue)
                     result = board.getValidSteps().get(p);
-                /*if(depth == MAX_DEPTH && value == maxValue && new Random().nextInt(2) == 1)
+                /*if(depth == MAX_DEPTH && value == maxValue && random.nextInt(2) == 0)
                     result = board.getValidSteps().get(p);*/
                 maxValue = Math.max(maxValue, value);
                 alpha = Math.max(alpha, value);
@@ -69,6 +97,7 @@ public class StudentPlayer extends Player{
 
     @Override
     public int step(Board board) {
+        //System.out.println(minimax(board, MAX_DEPTH, Integer.MIN_VALUE, Integer.MAX_VALUE, true));
         minimax(board, MAX_DEPTH, Integer.MIN_VALUE, Integer.MAX_VALUE, true);
         return result;
     }
